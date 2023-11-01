45ef9d36ed640cc1dc49e940ac38fc756571b3d5
reStar-zero <crygstar@gmail.com>
2023-11-01 23:38:13 +0300

    модель и персонажи
    добавлены модель и персонажи

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -19,13 +19,13 @@ public class Co : MonoBehaviour
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        
+        float v = Input.GetAxis("Vertical");    
+
         Vector3 directionVector = new Vector3(h,0,v);
         if (directionVector.magnitude > Mathf.Abs(0.05f))   
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), Time.deltaTime * 10);
 
-        animator.SetFloat("Speed", Vector3.ClampMagnitude(directionVector, 1).magnitude);
+        animator.SetFloat("Speed", Vector3.ClampMagnitude(directionVector, 1).magnitude);      
         rigidbody.velocity = Vector3.ClampMagnitude(directionVector,1) * speed;
     }
 }

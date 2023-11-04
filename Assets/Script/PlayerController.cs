dba330147df6d1e52c5e30c82f983715b32d3c22
reStar-zero <crygstar@gmail.com>
2023-11-04 19:49:08 +0300

    камера и передвижение
    Добавлена камера и передвижение

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,27 +5,45 @@ using UnityEngine;
 public class Co : MonoBehaviour
 {
     private Animator animator;
-    private Rigidbody rigidbody;
-    public float rotationSpeed = 10;
+    private Rigidbody rigidbody;   
+    public Transform firstCamera;
     public float speed = 2f;
-    // Start is called before the first frame update
+    //public CharacterController controller;
+    public float smoothTime;
+    float smooth;
+
     void Start()
     {
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();   
     }
 
-    // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");    
+        float v = Input.GetAxis("Vertical");
+        Vector3 directionVector = new Vector3(h,0,v).normalized;
 
-        Vector3 directionVector = new Vector3(h,0,v);
-        if (directionVector.magnitude > Mathf.Abs(0.05f))   
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), Time.deltaTime * 10);
+        if (directionVector.magnitude > Mathf.Abs(0.05f))
+        {
+            float rotation = Mathf.Atan2(directionVector.x, directionVector.z) * Mathf.Rad2Deg + firstCamera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref smooth, smoothTime);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), Time.deltaTime * 10);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 move = Quaternion.Euler(0f, rotation, 0f) * Vector3.forward;
 
-        animator.SetFloat("Speed", Vector3.ClampMagnitude(directionVector, 1).magnitude);      
+            //controller.Move(move.normalized * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+
+        }
+        else
+        {
+
+        }
+        animator.SetFloat("InputMagnitude", Vector3.ClampMagnitude(directionVector,1).magnitude);
         rigidbody.velocity = Vector3.ClampMagnitude(directionVector,1) * speed;
     }
 }

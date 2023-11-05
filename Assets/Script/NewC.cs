0bf88f77cc803a931b69f3c8bb3d5edd8934548b
reStar-zero <crygstar@gmail.com>
2023-11-05 16:28:27 +0300

    ничего не добавлено
    ничего

--- Assets/Script/NewC.cs
+++ Assets/Script/NewC.cs
@@ -2,40 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Co : MonoBehaviour
+public class NewC : MonoBehaviour
 {
     private Animator animator;
-    private Rigidbody rigidbody;   
+    private Rigidbody rigidbody;
     public Transform firstCamera;
     public float speed = 2f;
-    //public CharacterController controller;
     public float smoothTime;
     float smooth;
 
-    void Start()
+    private void Start()
     {
         animator = GetComponent<Animator>();
-        rigidbody = GetComponent<Rigidbody>();   
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
-    {   
+    {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        Vector3 directionVector = new Vector3(h,0,v).normalized;
+        Vector3 directionVector = new Vector3(h, 0, v).normalized;
+
 
         if (directionVector.magnitude > Mathf.Abs(0.05f))
         {
             float rotation = Mathf.Atan2(directionVector.x, directionVector.z) * Mathf.Rad2Deg + firstCamera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref smooth, smoothTime);
-            //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), Time.deltaTime * 10);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Vector3 move = Quaternion.Euler(0f, rotation, 0f) * Vector3.forward;
+            Vector3 move = Quaternion.Euler(0f, rotation, 0f) * Vector3.forward; 
 
-            //controller.Move(move.normalized * speed * Time.deltaTime);
         }
-
-        animator.SetFloat("InputMagnitude", directionVector.magnitude);
-        rigidbody.velocity = directionVector * speed;
+        animator.SetFloat("InputMagnitude", Vector3.ClampMagnitude(directionVector, 1).magnitude);
+        rigidbody.velocity = Vector3.ClampMagnitude(directionVector, 1) * speed;
     }
 }
